31b37b7e05dbeaabd34e242687180b7204e83c58
digiabhi <singhabhishek111213@gmail.com>
2025-09-26 15:50:02 +0530

    Added crud repository

---
+++ Backend/src/repositories/user.repository.js
@@ -0,0 +1,16 @@
+import User from "../schema/user.schema.js";
+import crudRepository from "./crud.repository.js";
+
+const userRepository = {
+    ...crudRepository(User),
+    getByEmail: async function(email){
+        const user = await User.findOne({ email });
+        return user;
+    },
+    getByUsername: async function(username){
+        const user = await User.findOne({ username });
+        return user;
+    }
+};
+
+export default userRepository;
